9efdb2c74c76ad8056cee60dadd2d7efab0912ab
Alisson Dias <alissonsalinos@outlook.com>
2021-02-15 09:14:30 -0300

    Open Graph 2

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>GitHub UserSearch</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Quicksand" rel="stylesheet" />
         <meta property="og:url" content="https://github-usersearch.alissonsalinos.vercel.app" key="ogurl" />
         <meta property="og:title" content="GitHub UserSearch" key="ogtitle" />
